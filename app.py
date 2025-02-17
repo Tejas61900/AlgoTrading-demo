3acf0eab7d3dbed0e292652338672b1764795658
Tejas <73832085+Tejas61900@users.noreply.github.com>
2025-02-17 19:29:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def run_backtest():
     return render_template('index.html', results=results_html)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
